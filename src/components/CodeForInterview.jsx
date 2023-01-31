d0ffa56f0580b06c43b8e556dee88c0bd51cd486
Htoo Maung Thait <htoomaungthait@gmail.com>
2023-02-01 06:22:13 +0630

    code interview page setup

--- src/components/CodeForInterview.jsx
+++ src/components/CodeForInterview.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Box, Typography, styled } from "@mui/material";
+import Youtube from "../assets/images/youtube_channel.png";
+import InstaTele from "../assets/images/InstaTele.jpeg";
+
+const Header = styled(Box)`
+margin: 50px;
+& > div {
+    margin-top: 50px;
+}
+`;
+
+const Image = styled("img")({
+    width: "50%",
+    height: "50%",
+});
 
 const CodeForInterview = () => {
-  return (
+    return (
+        <Header sx={{ mt: 10 }}>
+            <Typography variant="h4" >Code for Interview</Typography>
+            <Box style={{ display: "flex" }}>
+                <Image src={Youtube} />
+                <Image src={InstaTele} />
+            </Box>
+        </Header>
+    );
+};
 
-    <div>
-        <br/> <br/> <br/>
-        <h1> CodeForInterview </h1>
-    </div>
-  )
-}
-
-export default CodeForInterview
+export default CodeForInterview;
