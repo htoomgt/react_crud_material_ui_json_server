2d241e817ddc85a6c4c8ab3ef92deec119bbb63d
Htoo Maung Thait <htoomaungthait@gmail.com>
2023-02-05 22:45:01 +0630

    can make edit now

--- src/components/EditUser.jsx
+++ src/components/EditUser.jsx
@@ -1,11 +1,81 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
+import { FormGroup, FormControl, InputLabel, Input, Button, styled, Typography } from '@mui/material';
+import { useNavigate, useParams } from 'react-router-dom';
+import { getUsers, editUser } from '../services/api';
+
+const  initialValue = {
+  name : '',
+  username : '',
+  email :  '',
+  phone : ''
+}
+
+const  Container = styled(FormGroup)`
+  width: 50%;
+  margin:  5% 0 0 25%;
+  & > div {
+    margin-top: 20px;
+  }
+`;
+
+
+
 
 const EditUser = () => {
+  const [user, setUser]  = useState(initialValue);
+  const {name, username, email, phone} = user;
+  const {id}   = useParams();
+
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    loadUserDetails();
+  }, []);
+
+  const loadUserDetails =  async() => {
+    const response = await getUsers(id);
+    setUser(response.data);
+  }
+
+  const editUserDetails = async () => {
+    await  editUser(id, user);
+    navigate('/all');
+  }
+
+  const onValueChange = (e) => {
+    console.log(e.target.value);
+    setUser({...user, [e.target.name] : e.target.value})
+  }
+
+
   return (
-    <div>
-        <br/> <br/> <br/>
-        <h1> EditUser </h1>
-    </div>
+    <Container>
+      <Typography variant="h4"> Edit Information</Typography>
+      <FormControl>
+        <InputLabel htmlFor="txtName"> Name</InputLabel>
+        <Input onChange={(e) => onValueChange(e)} name="name"  value={name}  id="txtName" aria-describedby="my-helper-text" />
+      </FormControl>
+
+      <FormControl>
+        <InputLabel htmlFor="txtUsername"> Username</InputLabel>
+        <Input onChange={(e) => onValueChange(e)} name="username"  value={username}  id="txtUsername" aria-describedby="my-helper-text" />
+      </FormControl>
+
+      <FormControl>
+        <InputLabel htmlFor="txtUsername"> Email</InputLabel>
+        <Input onChange={(e) => onValueChange(e)} name="email"  value={email}  id="txtEmail" aria-describedby="my-helper-text" />
+      </FormControl>
+
+      <FormControl>
+        <InputLabel htmlFor="txtUsername"> Phone</InputLabel>
+        <Input onChange={(e) => onValueChange(e)} name="phone"  value={phone}  id="txtPhone" aria-describedby="my-helper-text" />
+      </FormControl>
+
+      <FormControl>
+        <Button variant="contained" color="primary" onClick={()=> editUserDetails()}>Edit User</Button>
+      </FormControl>
+    </Container>
     
   )
 }
